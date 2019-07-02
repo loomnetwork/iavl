acb301c14519d1914e06db5ea4ee04bfccd0a086
Matthew Campbell <hyper@hyperworks.nu>
2019-07-02 15:35:33 +0700

    cleanup unneeded logging

--- mutable_tree.go
+++ mutable_tree.go
@@ -375,7 +375,6 @@ func (tree *MutableTree) SaveVersionMem() ([]byte, int64, error) {
 }
 
 // FlushMemDisk saves a new tree to disk and removes all the versions in memory
-// TODO keep some in an LRU Cache
 func (tree *MutableTree) FlushMemVersionDisk() ([]byte, int64, error) {
 	x, y, err := tree.saveVersion(true)
 	nextTimeClear = true
